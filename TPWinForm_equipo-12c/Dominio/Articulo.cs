d16f0e71472e28dbaa4964f58312bbbdf9d7535d
Cristian Sanchez <c.sanchezmdp@gmail.com>
2026-04-20 23:02:08 -0300

    descomentar campo categoria.

--- TPWinForm_equipo-12c/Dominio/Articulo.cs
+++ TPWinForm_equipo-12c/Dominio/Articulo.cs
@@ -17,7 +17,7 @@ namespace Dominio
 
         public string Descripcion { get; set; }
 
-        //public Marca Marca { get; set; }
+        public Marca Marca { get; set; }
 
         //public Categoria Categoria { get; set; }
 
