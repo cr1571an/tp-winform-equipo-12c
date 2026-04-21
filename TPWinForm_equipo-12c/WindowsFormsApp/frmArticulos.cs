0b440f3009aa37838dd6947705fea005e986a5a3
Ulises Aguirre <Aguirre.ulises2981@gmail.com>
2026-04-21 03:25:07 -0300

    Se agregó las clases Categoria, CategoriaNegocio y frmArticulos.

---
+++ TPWinForm_equipo-12c/WindowsFormsApp/frmArticulos.cs
@@ -0,0 +1,27 @@
+﻿using Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class frmArticulos : Form
+    {
+        public frmArticulos()
+        {
+            InitializeComponent();
+        }
+
+        private void frmArticulos_Load(object sender, EventArgs e)
+        {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            dataGridView1.DataSource = negocio.Listar();
+        }
+    }
+}
