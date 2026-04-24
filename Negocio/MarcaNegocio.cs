c26fb06413a92bd8f47f4206ecb74000a5ca5d67
Ulises Aguirre <Aguirre.ulises2981@gmail.com>
2026-04-24 03:24:42 -0300

    Agregando ABM de Marca (alta, baja y modificación)

--- Negocio/MarcaNegocio.cs
+++ Negocio/MarcaNegocio.cs
@@ -39,5 +39,65 @@ namespace Negocio
                 datos.cerrarConexion();
             }
         }
+        public void agregar(Marca marca)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("Insert into MARCAS (Descripcion) values (@descripcion);");
+                datos.setearParametro("@descripcion", marca.Descripcion);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificar(Marca marca)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("UPDATE MARCAS SET Descripcion = @descripcion WHERE Id = @id;");
+                datos.setearParametro("@id", marca.Id);
+                datos.setearParametro("@descripcion", marca.Descripcion);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("DELETE FROM MARCAS WHERE Id = @id;");
+                datos.setearParametro("@id", id);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
     }
 }
